306fc02151cab2bf4e0b0b5fb4f4fd05c48fe43e
huangxiaoyan <hxysnail@gmail.com>
2018-11-27 14:59:12 +0800

    fix quicksort code

--- src/javascript/sorting/quick-sort/quick-sort.js
+++ src/javascript/sorting/quick-sort/quick-sort.js
@@ -1,22 +1,22 @@
 function quickSort(arr) {
-  if (arr.length <= 1) {
-    return arr
-  }
-
-  var pivot = arr[0]
-  var leftArr = []
-  var rightArr = []
-
-  for(var i=0; i<arr.length; i++) {
-    if (arr[i] < pivot) {
-      leftArr.push(arr[i])
-    } else if (arr[i] > pivot) {
-      rightArr.push(arr[i])
+    if (arr.length <= 1) {
+        return arr
     }
-  }
 
-  leftArr = quickSort(leftArr)
-  rightArr = quickSort(rightArr)
+    var pivot = arr[0]
+    var leftArr = []
+    var rightArr = []
 
-  return [].concat(leftArr, [pivot], rightArr)
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            leftArr.push(arr[i])
+        } else if (arr[i] > pivot) {
+            rightArr.push(arr[i])
+        }
+    }
+
+    leftArr = quickSort(leftArr)
+    rightArr = quickSort(rightArr)
+
+    return [].concat(leftArr, [pivot], rightArr)
 }
